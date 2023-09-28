16834c8d9430c46f60c515d1a7bd41265ee15c09
Jake Oliver <jacob.mj.oliver@gmail.com>
2023-09-28 15:46:25 +0100

    build(core.py): add core.py to configure model

---
+++ regression_model/config/core.py
@@ -0,0 +1,63 @@
+"""Core file for operation of regression_model."""
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseModel
+from strictyaml import YAML, load
+
+import regression_model
+
+# Project Directories
+PACKAGE_ROOT = Path(regression_model.__file__).resolve().parent
+ROOT = PACKAGE_ROOT.parent
+CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
+DATASET_DIR = PACKAGE_ROOT / "datasets"
+TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+
+
+class AppConfig(BaseModel):
+    """Application-level config."""
+
+    package_name: str
+    training_data_file: str
+    test_data_file: str
+    pipeline_save_file: str
+
+
+class Config(BaseModel):
+    """Master config object."""
+
+    app_config: AppConfig
+
+
+def find_config_file() -> Path:
+    """Return filepath to configuration file."""
+    if CONFIG_FILE_PATH.is_file():
+        return CONFIG_FILE_PATH
+    raise Exception(f"Config not found at {CONFIG_FILE_PATH!r}")
+
+
+def fetch_config_from_yaml(cfg_path: Optional[Path] = None) -> YAML:
+    """Parse YAML containing the package configuration."""
+    if not cfg_path:
+        cfg_path = find_config_file()
+
+    if cfg_path:
+        with open(cfg_path, "r") as conf_file:
+            parsed_config = load(conf_file.read())
+            return parsed_config
+    raise OSError(f"Did not find config file at path: {cfg_path}")
+
+
+def create_and_validate_config(parsed_config: YAML = None) -> Config:
+    """Run validation on config values."""
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml()
+
+    # specify the data attribute from the strictyaml YAML type
+    _config = Config(app_config=AppConfig(**parsed_config.data))
+
+    return _config
+
+
+config = create_and_validate_config()
