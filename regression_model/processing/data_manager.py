04db4e1a1165108bf92afdaac0cb2425a8ccfe7a
Jake Oliver <jacob.mj.oliver@gmail.com>
2023-10-10 15:54:48 +0100

    build(data_manager.py): Move from oauth2client to google-auth

--- regression_model/processing/data_manager.py
+++ regression_model/processing/data_manager.py
@@ -9,8 +9,8 @@ import requests
 from sklearn.pipeline import Pipeline
 import joblib
 import typing as t
-import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -19,26 +19,10 @@ from regression_model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
 
 start_year = 2018
 url = "https://docs.google.com/spreadsheets/d/1w-LyhsGUqNFiFqsF2QVgfA5MARTMRWFxMbG-FiWQf9c/edit#gid=621622277"
+SPREADSHEET_ID = "1w-LyhsGUqNFiFqsF2QVgfA5MARTMRWFxMbG-FiWQf9c"
 
 
-def getdays(*, date: str) -> int:
-    """Calculate difference in days between today and given date."""
-    test_date = date
-    temp_test_dates = test_date.split("/")
-    test_dates = [int(i) for i in temp_test_dates]
-
-    today = datetime.date.today()
-    today_date = today.strftime("%d/%m/%Y")
-    temp_today_dates = today_date.split("/")
-    today_dates = [int(i) for i in temp_today_dates]
-
-    d0 = datetime.date(test_dates[2], test_dates[1], test_dates[0])
-    d1 = datetime.date(today_dates[2], today_dates[1], today_dates[0])
-    delta = d1 - d0
-    return delta.days
-
-
-def load_dataset(*, file_name: str) -> pd.DataFrame:
+def load_dataset(file_name: str) -> pd.DataFrame:
     """
     Load movie dataset into input_df.
 
@@ -55,7 +39,7 @@ def load_dataset(*, file_name: str) -> pd.DataFrame:
             return create_moviedb(file_name)
 
 
-def create_moviedb(*, file_name: str, test_file: bool = False) -> pd.DataFrame:
+def create_moviedb(file_name: str, test_file: bool = False) -> pd.DataFrame:
     """Create the movie dataset if it does not exist.
 
     pull_from_sheet() creates a single dataframe containing all user data
@@ -72,9 +56,9 @@ def create_moviedb(*, file_name: str, test_file: bool = False) -> pd.DataFrame:
     return moviedb
 
 
-def pull_from_sheet(*, test_file: bool = False) -> pd.DataFrame:
+def pull_from_sheet(test_file: bool = False) -> pd.DataFrame:
     """Pull input data and concatenate into single sheet."""
-    # Authenticate with the google sheet
+    # Authenticate with google sheet
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/spreadsheets",
@@ -85,7 +69,12 @@ def pull_from_sheet(*, test_file: bool = False) -> pd.DataFrame:
     creds = ServiceAccountCredentials.from_json_keyfile_name(
         "service_account.json", scope
     )
-    client = gspread.authorize(creds)
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        in_sheets = service.spreadsheets()
+    except:
+        raise AttributeError("Could not authenticate with google sheet")
 
     # Obtain the current year and number of sheets that should exist
     today = datetime.date.today()
@@ -97,26 +86,32 @@ def pull_from_sheet(*, test_file: bool = False) -> pd.DataFrame:
     spreadsheet_df_list = []
 
     for i in range(0, year_diff + 1):
-        year = int(start_year) + 1
+        year = int(start_year) + i
+        sheet_title = "Films " + str(year)
 
-        sheet_title = "Films " + year
-        sheet = client.open_by_url(url).worksheet(sheet_title)
-        json_sheet = sheet.get_all_records()
-        json_object = json.dumps(json_sheet)
+        # Get values of each sheet using sheet_title
+        result = (
+            in_sheets.values()
+            .get(spreadsheetId=SPREADSHEET_ID, range=f"{sheet_title}!A1:N101")
+            .execute()
+        )
 
-        spreadsheet_df_list.append(pd.read_json(json_object))
+        # First list item is columns, rest is data
+        spreadsheet_df_list.append(
+            pd.DataFrame(result["values"][1:], columns=result["values"][0])
+        )
 
     # Concatenate the list of dfs into a single dataframe
-    final_df = pd.concat(spreadsheet_df_list)
+    final_df = pd.concat(spreadsheet_df_list).dropna()
 
     # If making test file, only return 10 random rows
     if test_file:
-        final_df.sample(10, random_state=42)
+        return final_df.sample(10, random_state=42)
     else:
         return final_df
 
 
-def pull_from_imdb(*, in_df: pd.DataFrame) -> pd.DataFrame:
+def pull_from_imdb(in_df: pd.DataFrame) -> pd.DataFrame:
     """
     Scrape imdb for movie data for each film using omdb.
 
@@ -157,9 +152,7 @@ def pull_from_imdb(*, in_df: pd.DataFrame) -> pd.DataFrame:
     return update_imdb_df(out_df, in_df)
 
 
-def update_imdb_df(
-    *, imdb_df: pd.DataFrame, spreadsheet_df: pd.DataFrame
-) -> pd.DataFrame:
+def update_imdb_df(imdb_df: pd.DataFrame, spreadsheet_df: pd.DataFrame) -> pd.DataFrame:
     """Add additional fields from spreadsheet to df created from omdb data."""
     imdb_df["Format"] = spreadsheet_df["Format"].values
     imdb_df["Viewing Date"] = spreadsheet_df["Viewing Data"].values
@@ -196,7 +189,24 @@ def year_search_string(self, date):
     return str(dates[-1])
 
 
-def save_pipeline(*, pipeline_to_persist: Pipeline) -> None:
+def getdays(date: str) -> int:
+    """Calculate difference in days between today and given date."""
+    test_date = date
+    temp_test_dates = test_date.split("/")
+    test_dates = [int(i) for i in temp_test_dates]
+
+    today = datetime.date.today()
+    today_date = today.strftime("%d/%m/%Y")
+    temp_today_dates = today_date.split("/")
+    today_dates = [int(i) for i in temp_today_dates]
+
+    d0 = datetime.date(test_dates[2], test_dates[1], test_dates[0])
+    d1 = datetime.date(today_dates[2], today_dates[1], today_dates[0])
+    delta = d1 - d0
+    return delta.days
+
+
+def save_pipeline(pipeline_to_persist: Pipeline) -> None:
     """Persist the pipeline.
 
     Save the versioned model, and overwrite any previously saved models. This
@@ -211,14 +221,14 @@ def save_pipeline(*, pipeline_to_persist: Pipeline) -> None:
     joblib.dump(pipeline_to_persist, save_path)
 
 
-def load_pipeline(*, file_name: str) -> Pipeline:
+def load_pipeline(file_name: str) -> Pipeline:
     """Load the persisted pipeline."""
     file_path = TRAINED_MODEL_DIR / file_name
     trained_model = joblib.load(filenmae=file_path)
     return trained_model
 
 
-def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
+def remove_old_pipelines(files_to_keep: t.List[str]) -> None:
     """Remove old model pipelines.
 
     This is to ensure there is a simple one-to-one mapping between the package
